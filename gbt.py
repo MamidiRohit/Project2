4cf514ea34d33a133cd9613194f43912a6db2308
notjiang <128391769+Hj006@users.noreply.github.com>
2024-11-08 23:37:44 -0600

    Completed the idea of how to write gbt class

--- gbt.py
+++ gbt.py
@@ -1,10 +1,31 @@
+import numpy as np
+
+
 class GBT():
+    '''
+    parameters:
+    for function fit, get the inputed training data X and target variable y
+    init fuction needs learning rate η, and the maximum depth d of the decision tree model
+    
+    initialize the model : let F0(x) = 0
+    then, need a loop , in the loop performing:
+        calculate the residuals of the loss function rmi: rmi = yi - Fm-1(xi), where xi and yi are the features and target value of the i-th sample, respectively
+        train a regression tree Gm(X) and predict the residual value for each xi
+        minimize the mean square error of each node to find regression coefficients of leaf nodes
+        update the model: Fm(x) = Fm-1(x) + ηgammamGm(x)
+    return final model
+    '''
+
     def __init__(self,loss,learning_rate=0.01,max_depth=5,min_split=2):
         self.learning_rate = learning_rate
         self.max_depth = max_depth
         self.min_split = min_split
         self.loss = loss
-    def fit():
+    def fit(self,X,y):
+        y_pred = np.zeros(len(y))#用于存储新的预测值
+        for i in range():
+            tree = RgressionTree()
+
         pass
     def predict():
         pass
